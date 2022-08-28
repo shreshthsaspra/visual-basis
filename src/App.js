50017e98cf95d6540ab3ace203eabcd19cc35d1e
HARSH TRIPATHI <1940317@sliet.ac.in>
2022-08-28 11:32:19 +0530

    Deploying on Github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HospitalLogin from './pages/Login/HospitalLogin';
+import DoctorLogin from './pages/Login/DoctorLogin';
+import StaffManagement from './pages/Login/StaffManagement';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Router>
+    <Routes>
+         <Route  path="/" element={<HospitalLogin />} />
+         <Route  path="/doctor-login" element={<DoctorLogin />} />
+         <Route  path="/staff-login" element={<StaffManagement />} />
+
+
+         
+         
+    </Routes>
+    </Router>
+    
+    </>
   );
 }
 
---
+++ src/pages/Login/DoctorLogin.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import styles from './DoctorLogin.module.css';
+import Logo from '../../assests/demoLogo.png';
+import UserName from '../../assests/USER.png';
+import Password from '../../assests/password.png'
+const DoctorLogin = () => {
+  return (
+    <>
+     <div className={styles.loginSection}>
+                <div className={styles.logoSection}>
+                    <img src={Logo} alt="Dummy Logo" />
+                    <p>Doctor</p>
+                </div>
+                <div className={styles.loginOuter}>
+                    <div className={styles.loginCard}>
+                        <form className={styles.loginForm}>
+                            <div className={styles.wrap}>
+                                <img className={styles.inputIcon} src={UserName} alt="User Icon" />
+                                <input type="text" placeholder='Username' />
+                            </div>
+
+                            <div className={styles.wrap}>
+                                <img className={styles.inputIcon} src={Password} alt="Password Icon" />
+                                <input type="text" placeholder='Password' />
+                            </div>
+                            <button className={styles.button}>LOGIN</button>
+                            <div className={styles.bottom}>
+                                <p>Create Account</p>
+                                <p>Need Help?</p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+    </>
+  )
+}
+
+export default DoctorLogin
