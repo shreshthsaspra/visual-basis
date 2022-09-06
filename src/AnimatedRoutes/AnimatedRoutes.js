94deb79e367d88f8e325d31ebd236083489ce806
HARSH TRIPATHI <1940317@sliet.ac.in>
2022-09-06 14:29:01 +0530

    3d model

--- src/AnimatedRoutes/AnimatedRoutes.js
+++ src/AnimatedRoutes/AnimatedRoutes.js
@@ -8,6 +8,7 @@ import Home from '../pages/Hospital/Home/Home';
 import { AnimatePresence } from 'framer-motion';
 import styles from "./AnimatedRoutes.module.css"  
 import PateintsSearch from '../pages/Hospital/PateintsSearch/PateintsSearch';
+import ThreedModel from '../pages/ThreedModel';
 
 function AnimatedRoutes() {
   const location = useLocation()
@@ -17,6 +18,8 @@ function AnimatedRoutes() {
         <Routes location={location} key = { location.pathname}>
           <Route path="/" element={<HospitalLogin />} />
           <Route path="/Hospital" element={<Home/>} />
+          <Route path="/threed" element={<ThreedModel/>} />
+
           <Route path="/doctor-login" element={<DoctorLogin />} />
           <Route path="/Hospital/pateintmanagement" element={<PateintRegister/>} />
           <Route path="/Hospital/PateintSearch" element={<PateintsSearch/>} />
---
+++ src/pages/ThreedModel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Threed.css'
+
+const ThreedModel = () => {
+  return (
+    <>
+    <h2 className='heading'>Hello from 3d Model</h2>
+
+    <div id="messages">
+    <a id="head" href="#">Fly to Head</a>
+    <a id="foot" href="#">Fly to Foot</a>
+    <a id="left" href="#">Rotate Left</a>
+    <a id="right" href="#">Rotate Right</a>
+</div>
+    </>
+  )
+}
+
+export default ThreedModel
