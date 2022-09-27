f5b980c988dda1322f383d4befedf6750a2adfb5
Pravin Kumar <2030111@sliet.ac.in>
2022-09-27 22:08:29 +0530

    no changes

--- src/Storage/contextProvider.js
+++ src/Storage/contextProvider.js
@@ -7,30 +7,7 @@ export const DataStorage = ({children}) => {
   
 const [tokens, setTokens] = useState()
 const [PateintDetails, setPateintDetails] = useState([]);
-    let contextData = {
-        // user:user,
-        // authTokens:authTokens,
-        // loginUser:loginUser,
-        // logoutUser:logoutUser,
-    }
 
-
-    // useEffect(()=> {
-
-    //     if(loading){
-    //         updateToken()
-    //     }
-
-    //     let fourMinutes = 1000 * 60 * 4
-
-    //     let interval =  setInterval(()=> {
-    //         if(authTokens){
-    //             updateToken()
-    //         }
-    //     }, fourMinutes)
-    //     return ()=> clearInterval(interval)
-
-    // }, [authTokens, loading])
 
     return(
         <GlobalStorage.Provider value={{tokens, setTokens,PateintDetails, setPateintDetails}} >
