c63bfbb053f73a93f83b1ddbe98e37f696722ffd
HARSH TRIPATHI <1940317@sliet.ac.in>
2022-09-30 21:46:05 +0530

    cropping

--- src/components/UploadImage/PateintUploadImg.js
+++ src/components/UploadImage/PateintUploadImg.js
@@ -12,6 +12,9 @@ function PateintUploadImg() {
     const [croppedArea, setCroppedArea] = useState(null)
     const [initialCroppedArea, setInitialCroppedArea] = useState(undefined)
     const [scaleId, setScale] = useState(1)
+
+  const [horizontaloffset, setHorizontaloffset] = useState(0);
+
     function getRadianAngle(degreeValue) {
         return (degreeValue * Math.PI) / 180;
     }
@@ -30,78 +33,102 @@ function PateintUploadImg() {
             image.src = url;
         });
 
-    async function getCroppedImg(imageSrc, pixelCrop, rotation = 0) {
-        const image = await createImage(imageSrc);
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
+    // async function getCroppedImg(imageSrc, pixelCrop, rotation = 0) {
+    //     const image = await createImage(imageSrc);
+    //     const canvas = document.createElement("canvas");
+    //     const ctx = canvas.getContext("2d");
 
-        const maxSize = Math.max(image.width, image.height);
-        const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2));
+    //     const maxSize = Math.max(image.width, image.height);
+    //     const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2));
 
-        // set each dimensions to double largest dimension to allow for a safe area for the
-        // image to rotate in without being clipped by canvas context
-        canvas.width = safeArea;
-        canvas.height = safeArea;
+    //     // set each dimensions to double largest dimension to allow for a safe area for the
+    //     // image to rotate in without being clipped by canvas context
+    //     canvas.width = safeArea;
+    //     canvas.height = safeArea;
 
-        // translate canvas context to a central location on image to allow rotating around the center.
-        ctx.translate(safeArea / 2, safeArea / 2);
-        ctx.rotate(getRadianAngle(rotation));
-        ctx.translate(-safeArea / 2, -safeArea / 2);
+    //     // translate canvas context to a central location on image to allow rotating around the center.
+    //     ctx.translate(safeArea / 2, safeArea / 2);
+    //     ctx.rotate(getRadianAngle(rotation));
+    //     ctx.translate(-safeArea / 2, -safeArea / 2);
 
-        // draw rotated image and store data.
-        ctx.drawImage(
-            image,
-            safeArea / 2 - image.width * 0.5,
-            safeArea / 2 - image.height * 0.5
-        );
+    //     // draw rotated image and store data.
+    //     ctx.drawImage(
+    //         image,
+    //         safeArea / 2 - image.width * 0.5,
+    //         safeArea / 2 - image.height * 0.5
+    //     );
 
-        const data = ctx.getImageData(0, 0, safeArea, safeArea);
+    //     const data = ctx.getImageData(0, 0, safeArea, safeArea);
 
-        // set canvas width to final desired crop size - this will clear existing context
-        canvas.width = pixelCrop.width;
-        canvas.height = pixelCrop.height;
+    //     // set canvas width to final desired crop size - this will clear existing context
+    //     canvas.width = pixelCrop.width;
+    //     canvas.height = pixelCrop.height;
 
-        // paste generated rotate image with correct offsets for x,y crop values.
-        ctx.putImageData(
-            data,
-            0 - safeArea / 2 + image.width * 0.5 - pixelCrop.x,
-            0 - safeArea / 2 + image.height * 0.5 - pixelCrop.y
-        );
+    //     // paste generated rotate image with correct offsets for x,y crop values.
+    //     ctx.putImageData(
+    //         data,
+    //         0 - safeArea / 2 + image.width * 0.5 - pixelCrop.x,
+    //         0 - safeArea / 2 + image.height * 0.5 - pixelCrop.y
+    //     );
+    //     return canvas;
+    // }
+    // const onCropComplete = (croppedAreaPercentage, croppedAreaPixels) => {
+    //     setCroppedArea(croppedAreaPixels);
+    // };
+    // const generateDownload = async (imageSrc, crop) => {
+    //     if (!crop || !imageSrc) {
+    //         return;
+    //     }
+    //     const canvas = await getCroppedImg(imageSrc, crop);
+    //     canvas.toBlob(
+    //         (blob) => {
+    //             const previewUrl = window.URL.createObjectURL(blob);
+    //             console.log(blob);
+    //             const anchor = document.createElement("a");
+    //             anchor.download = "image.jpeg";
+    //             anchor.href = URL.createObjectURL(blob);
+    //             anchor.click();
 
-        // As Base64 string
-        // return canvas.toDataURL("image/jpeg");
-        return canvas;
-    }
-    const onCropComplete = (croppedAreaPercentage, croppedAreaPixels) => {
-        setCroppedArea(croppedAreaPixels);
-    };
-    const generateDownload = async (imageSrc, crop) => {
-        if (!crop || !imageSrc) {
-            return;
-        }
-
-        const canvas = await getCroppedImg(imageSrc, crop);
-
-        canvas.toBlob(
-            (blob) => {
-                const previewUrl = window.URL.createObjectURL(blob);
-                console.log(blob);
-                const anchor = document.createElement("a");
-                anchor.download = "image.jpeg";
-                anchor.href = URL.createObjectURL(blob);
-                anchor.click();
-
-                window.URL.revokeObjectURL(previewUrl);
-            },
-            "image/jpeg",
-            0.66
-        );
-    };
+    //             window.URL.revokeObjectURL(previewUrl);
+    //         },
+    //         "image/jpeg",
+    //         0.66
+    //     );
+    // };
 
     const handleZoomIn = () => {
         // setCrop(scaleId+0.4)
+        setScale(scaleId + 0.2)
+      }
+
+      console.log("Scale", scaleId);
+
+      const handleZoomOut = () => {
+        // setCrop(scaleId+0.4)
         setScale(scaleId - 0.2)
       }
+
+      const handleMoveBoxLeft = () => {
+
+        if (horizontaloffset > -500) {
+          setHorizontaloffset((prev) => prev - 20);
+          console.log("Horizontal Offset", horizontaloffset);
+        }
+    
+    
+      };
+    
+      const handleMoveBoxRight = () => {
+    
+        if (horizontaloffset < 500) {
+    
+          setHorizontaloffset((prev) => prev + 20);
+          console.log("Horizontal Offset", horizontaloffset);
+        }
+    
+    
+    
+      };
     return (
         <>
 
@@ -133,23 +160,32 @@ function PateintUploadImg() {
                                             position: "absolute",
                                             height: "500px",
                                             width: "375px",
-                                              left: 0,
+                                              left: horizontaloffset,
                                               right: 0,
                                               overflow:'hidden',
                                             boxShadow: "1000px 1000px 1000px 1000px rgba(255, 255, 255, 0.304)",
-                                            zIndex:101
-
+                                            zIndex:101,
                                         }}
                                     ></div>
                                     <img src={url} style={{ transform: `scale(${scaleId})` }} alt="" />
                                 </div>
                             </div>
 
-                            <button onClick={handleZoomIn}>Zoom</button>
+                          
+
+
                         </div>
 
                     </div>
-                    <button onClick={() => generateDownload(url, croppedArea)}>download</button>
+                    {/* <button onClick={() => generateDownload(url, croppedArea)}>download</button> */}
+
+                            {/* <button onClick={handleZoomIn}>ZoomIn</button>
+                            <button onClick={handleZoomOut}>ZoomOut</button> */}
+
+                            <button onClick={handleMoveBoxLeft}>Left</button>
+                            <button onClick={handleMoveBoxRight}>Right</button>
+
+
                 </div>
 
 
