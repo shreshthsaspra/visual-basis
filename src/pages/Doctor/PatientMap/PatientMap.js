525ecbbb741fb7d048900fdfdd598ff995dbb7e3
HARSH TRIPATHI <1940317@sliet.ac.in>
2022-10-03 15:07:28 +0530

    Add point

--- src/pages/Doctor/PatientMap/PatientMap.js
+++ src/pages/Doctor/PatientMap/PatientMap.js
@@ -7,165 +7,188 @@ import GlobalStorage from '../../../Storage/ContextProvider';
 const mapPoins = {
     "status": "success",
     "message": {
-      "keypoints": [
-        [
-          163,
-          76
-        ],
-        [
-          168,
-          68
-        ],
-        [
-          172,
-          68
-        ],
-        [
-          176,
-          68
-        ],
-        [
-          159,
-          68
-        ],
-        [
-          156,
-          68
-        ],
-        [
-          154,
-          68
-        ],
-        [
-          179,
-          71
-        ],
-        [
-          151,
-          70
-        ],
-        [
-          169,
-          85
-        ],
-        [
-          159,
-          85
-        ],
-        [
-          211,
-          128
-        ],
-        [
-          124,
-          126
-        ],
-        [
-          221,
-          197
-        ],
-        [
-          107,
-          191
-        ],
-        [
-          220,
-          254
-        ],
-        [
-          107,
-          251
-        ],
-        [
-          220,
-          274
-        ],
-        [
-          107,
-          270
-        ],
-        [
-          216,
-          272
-        ],
-        [
-          111,
-          270
-        ],
-        [
-          214,
-          264
-        ],
-        [
-          114,
-          263
-        ],
-        [
-          188,
-          262
-        ],
-        [
-          139,
-          262
-        ],
-        [
-          189,
-          360
-        ],
-        [
-          140,
-          359
-        ],
-        [
-          187,
-          451
-        ],
-        [
-          142,
-          455
-        ],
-        [
-          183,
-          463
-        ],
-        [
-          146,
-          466
-        ],
-        [
-          192,
-          482
-        ],
-        [
-          137,
-          483
+        "keypoints": [
+            [
+                163,
+                76
+            ],
+            [
+                168,
+                68
+            ],
+            [
+                172,
+                68
+            ],
+            [
+                176,
+                68
+            ],
+            [
+                159,
+                68
+            ],
+            [
+                156,
+                68
+            ],
+            [
+                154,
+                68
+            ],
+            [
+                179,
+                71
+            ],
+            [
+                151,
+                70
+            ],
+            [
+                169,
+                85
+            ],
+            [
+                159,
+                85
+            ],
+            [
+                211,
+                128
+            ],
+            [
+                124,
+                126
+            ],
+            [
+                221,
+                197
+            ],
+            [
+                107,
+                191
+            ],
+            [
+                220,
+                254
+            ],
+            [
+                107,
+                251
+            ],
+            [
+                220,
+                274
+            ],
+            [
+                107,
+                270
+            ],
+            [
+                216,
+                272
+            ],
+            [
+                111,
+                270
+            ],
+            [
+                214,
+                264
+            ],
+            [
+                114,
+                263
+            ],
+            [
+                188,
+                262
+            ],
+            [
+                139,
+                262
+            ],
+            [
+                189,
+                360
+            ],
+            [
+                140,
+                359
+            ],
+            [
+                187,
+                451
+            ],
+            [
+                142,
+                455
+            ],
+            [
+                183,
+                463
+            ],
+            [
+                146,
+                466
+            ],
+            [
+                192,
+                482
+            ],
+            [
+                137,
+                483
+            ]
         ]
-      ]
     }
-  }
+}
 
 const PatientMap = () => {
-    const {pointMap, point, setPointMap} = useContext(GlobalStorage)
+    const { pointMap, point, setPointMap } = useContext(GlobalStorage)
     console.log("POINT MAP", pointMap);
+    const deleteMap = (i) => {
+        console.log("clicked", i);
+        let copyMap = [...pointMap.front];
+
+        copyMap.splice(i, 1);
+        setPointMap({ ...pointMap, front: copyMap })
+    }
+    console.log("9090", pointMap);
+
+    const handleClick = event => {
+        // setPointMap({...pointMap, "1" });
+      };
     return (
         <>
             <div className={styles.mainDiv}>
                 <div className={styles.wrap}>
-                    
+
                     <img src={point.front} alt="" />
                     {
-                        pointMap.front.map((p) => (
-                            <div style={{
-                                top:`${p[1]}px`,
-                                left:`${p[0]}px`
-                            }} className={styles.point}>
+                        pointMap.front?.map((p, i) => (
+                            <div
+                                draggable
+                                key={i}
+                                style={{
+                                    top: `${p[1]}px`,
+                                    left: `${p[0]}px`,
+                                    cursor: 'pointer',
+                                }}
+                                className={styles.point}
+                                onClick={() => deleteMap(i)}
+                            >
 
                             </div>
                         ))
 
                     }
-                    
+
                 </div>
+
+                <button onClick={handleClick}>Add Point</button>
+
+
             </div>
         </>
     )
