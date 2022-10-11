960c07ab708ddf8b829f93567006b69ae7dec805
HARSH TRIPATHI <1940317@sliet.ac.in>
2022-10-11 14:35:53 +0530

    Movenet Front issue fixing

--- src/pages/Doctor/EditSingle/movenet/Movenet.js
+++ src/pages/Doctor/EditSingle/movenet/Movenet.js
@@ -2,22 +2,29 @@ import React, { useContext, useState, useEffect } from 'react';
 import styles from '../metapipe/MetaPipe.module.css';
 import Logo from '../../../../assests/demoLogo.png'
 import ShadoeLogo from '../../../../assests/shadowLogo.png'
+import { BsArrowRightCircleFill, BsArrowLeftCircleFill } from "react-icons/bs";
 
 import GlobalStorage from '../../../../Storage/ContextProvider';
+import { transform } from 'framer-motion';
 
 
 const Movenet = () => {
 
+    // const { pointMap, point, setPointMap } = useContext(GlobalStorage);
     const { pointMap, point, setPointMap, pointMapMv, setPointMapMv } = useContext(GlobalStorage);
 
+
     const [x, setX] = useState(false)
-    const [ind, setIndex] = useState(163)
+    const [ind, setIndex] = useState("")
     const [Yvalu, setY] = useState(false)
     const [right, setRight] = useState(false)
     const [Down, setDown] = useState(false)
     const [scaleXY, setScaleXY] = useState(1);
     const [width, setWidth] = useState(null);
     const [height, setHeight] = useState(null);
+    const [enableAdd, setEnableAdd] = useState(false);
+    const [enableEdit, setEnableEdit] = useState(false);
+    const [enableDelete, setEnableDelete] = useState(false);
 
     const scaleCoordinates = (x, y) => {
 
@@ -42,13 +49,71 @@ const Movenet = () => {
 
     // console.log("POINT MAP", pointMap);
     const deleteMap = (i) => {
-        // console.log("clicked", i);
-        let copyMap = [...pointMap.front];
+        console.log("clicked", i);
+        let copyMap = [...pointMapMv.front];
 
         copyMap.splice(i, 1);
-        setPointMap({ ...pointMap, front: copyMap })
+        setPointMapMv({ ...pointMapMv, front: copyMap })
     }
 
+    const handlePoint = (i) => {
+        if (enableDelete) {
+            deleteMap(i)
+            console.log(enableAdd);
+        }
+        if (enableEdit) {
+            setIndex(i)
+            console.log("edit inmdex", i);
+        }
+
+    }
+
+    console.log("indexxxx", ind);
+    const handlePosition = (action) => {
+        console.log("action", action);
+        if (action === 'xl') {
+            const value = pointMapMv.front?.map((obj, i) => {
+                if (i === ind) {
+                    return { ...obj, 0: obj?.[0] - 1 }
+                }
+                return obj
+            })
+            console.log(x);
+            setPointMapMv({ ...pointMapMv.front, front: value })
+        }
+        if (action === 'xr') {
+            const value = pointMapMv.front?.map((obj, i) => {
+                if (i === ind) {
+                    return { ...obj, 0: obj?.[0] + 1 }
+                }
+                return obj
+            })
+            console.log(x);
+            setPointMapMv({ ...pointMapMv.front, front: value })
+
+
+        }
+        if (action === 'yu') {
+            const value = pointMapMv.front?.map((obj, i) => {
+                if (i === ind) {
+                    return { ...obj, 1: obj?.[1] - 1 }
+                }
+                return obj
+            })
+            console.log(x);
+            setPointMapMv({ ...pointMapMv.front, front: value })
+        }
+        if (action === 'yd') {
+            const value = pointMapMv.front?.map((obj, i) => {
+                if (i === ind) {
+                    return { ...obj, 1: obj?.[1] + 1 }
+                }
+                return obj
+            })
+            console.log(x);
+            setPointMapMv({ ...pointMapMv.front, front: value })
+        }
+    }
 
 
     useEffect(() => {
@@ -60,101 +125,159 @@ const Movenet = () => {
         setScaleXY(450 / height1);
         //alert('just ran')
     }, []);
+    const handleAdd = () => {
 
-    useEffect(() => {
-        const updatePoint = () => {
-            const value = pointMap.front?.map((obj, i) => {
-                if (i == ind) {
-                    return { ...obj, 0: obj?.[0] - 1 }
-                }
-                return obj
-            })
-            console.log(x);
-            setPointMap({ ...pointMap.front, front: value })
-        }
-        updatePoint()
-    }, [x])
-    useEffect(() => {
-        const updatePoint = () => {
-            const value = pointMap.front?.map((obj, i) => {
-                if (i == ind) {
-                    return { ...obj, 0: obj?.[0] + 1 }
-                }
-                return obj
-            })
-            console.log(x);
-            setPointMap({ ...pointMap.front, front: value })
-        }
-        updatePoint()
-    }, [right])
-    useEffect(() => {
-        const updatePoint = () => {
-            const value = pointMap.front?.map((obj, i) => {
-                if (i == ind) {
-                    return { ...obj, 1: obj?.[1] - 1 }
-                }
-                return obj
-            })
-            console.log(x);
-            setPointMap({ ...pointMap.front, front: value })
-        }
-        updatePoint()
-    }, [Yvalu])
-    useEffect(() => {
-        const updatePoint = () => {
-            const value = pointMap.front?.map((obj, i) => {
-                if (i == ind) {
-                    return { ...obj, 1: obj?.[1] + 1 }
-                }
-                return obj
-            })
-            console.log(x);
-            setPointMap({ ...pointMap.front, front: value })
-        }
-        updatePoint()
-    }, [Down])
+        setEnableDelete(false)
+        setEnableEdit(false)
+        setEnableAdd(true);
+        setIndex("")
+    };
+    const handleDelete = () => {
+        setEnableDelete(true)
+        setEnableEdit(false)
+        setEnableAdd(false);
+        setIndex("")
 
-    const AddLocationPoints = (e) => {
-        console.log('ON CLICK, clientX:', e.clientX)
+    }
+    const handleEdit = () => {
+        setEnableAdd(false);
+        setEnableDelete(false)
+        setEnableEdit(true)
     }
 
+    const addPoint = (e) => {
+        // offsetX
+        // :
+        // 65
+        // offsetY
+        // :
+        // 51
+        console.log(e);
+        const x = e.nativeEvent.offsetX 
+        const y = e.nativeEvent.offsetY
+        const elem = [x, y]
+        console.log(elem);
+        console.log(pointMapMv);
+        // spush(elem)
+        const copyEle = [...pointMapMv.front]
+        copyEle.push(elem)
+        console.log(copyEle);
+        setPointMapMv({ ...pointMapMv.front, front: copyEle })
+
+    }
 
     return (
         <>
 
             {/* <CameraComponent /> */}
-            
-            <div style={{overflow: 'hidden'}} className={styles.cameraMain}>
+
+            <div style={{ overflow: 'hidden' }} className={styles.cameraMain}>
                 <div className={styles.width}>
                     <div className={styles.nav}>
                         <div className={styles.navButton}>
                             <img src={ShadoeLogo} alt="" />
-                            <p>Camera Screen</p>
+                            <p>Camera Screen MP</p>
                         </div>
                     </div>
 
 
                     <div className={styles.mainBody}>
+                        <div className={styles.controlEdit}>
+                            <div className={styles.controlEditInner}>
+                                <button
+                                    onClick={() => { enableEdit && handlePosition('xl') }}
+                                    style={{
+                                        border: 'none',
+                                        outline: 'none',
+                                        background: '#185EB6',
+                                        width: '40px',
+                                        height: '35px',
+                                        borderRadius: '5px',
+                                        color: '#FFFFFF',
+                                        fontFamily: 'poppins-medium',
+                                        position: "absolute",
+                                        left: "0",
+
+
+                                    }}
+                                    className={styles.leftScale}
+                                >
+                                    <BsArrowRightCircleFill />
+                                </button>
+                                <button
+                                    onClick={() => { enableEdit && handlePosition('xr') }}
+                                    style={{
+                                        border: 'none',
+                                        outline: 'none',
+                                        background: '#185EB6',
+                                        width: '40px',
+                                        height: '35px',
+                                        borderRadius: '5px',
+                                        color: '#FFFFFF',
+                                        fontFamily: 'poppins-medium',
+                                        position: "absolute",
+                                        right: "-42px",
+                                    }}>
+                                    <BsArrowRightCircleFill />
+                                </button>
+                                <button
+                                    className={styles.topScale}
+                                    onClick={() => { enableEdit && handlePosition('yu') }}
+                                    style={{
+                                        border: 'none',
+                                        outline: 'none',
+                                        background: '#185EB6',
+                                        width: '40px',
+                                        height: '35px',
+                                        borderRadius: '5px',
+                                        color: '#FFFFFF',
+                                        fontFamily: 'poppins-medium',
+                                        position: "absolute",
+                                        left: "41px",
+                                        top: "-43px"
+                                    }}>
+                                    <BsArrowLeftCircleFill />
+                                </button>
+                                <button
+                                    onClick={() => { enableEdit && handlePosition('yd') }}
+                                    className={styles.bottomScale}
+                                    style={{
+                                        border: 'none',
+                                        outline: 'none',
+                                        background: '#185EB6',
+                                        width: '40px',
+                                        height: '35px',
+                                        borderRadius: '5px',
+                                        color: '#FFFFFF',
+                                        fontFamily: 'poppins-medium',
+                                        position: "absolute",
+                                        bottom: "-2px",
+                                        left: "42px"
+                                    }}>
+                                    <BsArrowLeftCircleFill />
+                                </button>
+
+
+                            </div>
+                        </div>
                         <div className={styles.inner}>
                             {/* <CameraComponent /> */}
                             <h3>Front</h3>
 
                             <div className={styles.cameraStyle}>
                                 <div style={{
-                                   
+
                                     display: 'flex',
                                     justifyContent: 'center',
                                     alignItems: 'center',
-                                    overflow:'hidden'
+                                    overflow: 'hidden'
                                 }} className="mt-4">
-                                    <div className={styles.centerLine}>
-                                        
-                                    </div>
                                     <div className={styles.wrap}>
-                                        <img src={point.front} alt="" id="patientId" style={{ transform: `scale(${scaleXY})` }} />
+                                        <img onClick={(e) => {enableAdd&&addPoint(e)}} src={point.front} alt="" id="patientId" style={{ transform: `scale(${scaleXY})` }} />
                                         {
                                             pointMapMv.front?.map((p, i) => (
                                                 <div
+                                               
                                                     draggable
                                                     key={i}
                                                     style={{
@@ -162,8 +285,8 @@ const Movenet = () => {
                                                         left: `${scaleCoordinates(p[0], p[1]).x}px`,
                                                         cursor: 'pointer',
                                                     }}
-                                                    className={styles.point}
-                                                // onClick={() => handleClick(i)}
+                                                    className={i === ind? styles.pointActive:styles.point}
+                                                onClick={(e) => {e.stopPropagation(); handlePoint(i)}}
                                                 >
 
                                                 </div>
@@ -177,9 +300,9 @@ const Movenet = () => {
                             </div>
 
                             <div className={styles.bottomButton}>
-                            <button onMouseMove={AddLocationPoints} >Add Points</button>
-                            <button>Edit Points</button>
-                            <button>Delete Points</button>
+                                <button className={enableAdd?styles.actiiveMode:""} onClick={() => handleAdd()}>Add Points</button>
+                                <button className={enableEdit?styles.actiiveMode:""} onClick={() => handleEdit()}>Edit Points</button>
+                                <button className={enableDelete?styles.actiiveMode:""} onClick={() => handleDelete()}>Delete Points</button>
 
 
                             </div>
